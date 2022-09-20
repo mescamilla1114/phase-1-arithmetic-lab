fff15058adb276e2f80550136ae0b3dab45d062c
Michael <mescamilla1114@gmail.com>
2022-09-20 12:56:31 -0500

    commit tree 8c65e0da648b71f6e7fa12cc1454131bdc4e22fb

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1*num2;
+
+function funMath(num1,num2,multiply){
+
+return(multiply);
+}
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+function randomMath(random){
+
+return(random);
+}
+
+let num3 = 7;
+let num4 = 11;
+
+let mod = num4 % num3;
+
+function modulo(mod){
+
+return(mod);
+}
+
+let max= Math.max(1, 2, 3, 4, 20);
+
+function maxMath(max){
+
+return(max);
+}
